741cba16068766910eb476b7d1ef6be442f68d2c
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2021-12-30 10:54:34 -0300

    modal para cerrar sesión listo

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react'
+
 const Navbar = () => {
+    const [classModal, setClassModal] = useState('modal')
     
     const handleNavbar = () => {
         document.getElementById('nav-menu')?.classList.toggle('is-active')
+    }
+
+    const handleOpenModal = () => {
+        setClassModal('modal is-active')
+    }
+
+    const handleCloseModal = () => {
+        setClassModal('modal is-clipped')
     }
 
     return (
@@ -23,11 +34,29 @@ const Navbar = () => {
                         <a className='navbar-item has-text-weight-semibold'>Mis reseñas</a>
                         <div className='navbar-item'>
                             <div className='buttons'>
-                                <a className='button is-danger'>
+                                <a className='button is-danger' onClick={handleOpenModal}>
                                     <strong>Cerrar Sesión</strong>
                                 </a>
                             </div>
                         </div>
+                    </div>
+                </div>
+
+                {/* Modal */}
+                <div className={classModal}>
+                    <div className="modal-background"></div>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                            <p className="modal-card-title has-text-weight-bold">Cerrar Sesión</p>
+                            <button className="delete" aria-label="close" onClick={handleCloseModal}></button>
+                        </header>
+                        <section className='modal-card-body'>
+                            <p className='has-text-dark has-text-weight-semibold'>¿Estás seguro de cerrar sesión?</p>
+                        </section>
+                        <footer className='modal-card-foot'>
+                            <button className='button is-link'>Cerrar Sesión</button>
+                            <button className='button' onClick={handleCloseModal}>Cancelar</button>
+                        </footer>
                     </div>
                 </div>
             </div>
